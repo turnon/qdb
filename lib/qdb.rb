96ce9dab2370ed5e9ec1bf6f6b3ca5415fbfcb96
ken <block24block@gmail.com>
2019-11-21 16:27:16 +0000

    create table

--- lib/qdb.rb
+++ lib/qdb.rb
@@ -1,6 +1,21 @@
 require "qdb/version"
+require 'active_record'
 
 module Qdb
-  class Error < StandardError; end
-  # Your code goes here...
+
+  class << self
+    def setup(path)
+      ActiveRecord::Base.establish_connection(adapter: 'sqlite3', database: path)
+    end
+
+    def create_table(name, &block)
+      ActiveRecord::Migration.new.create_table(name, &block)
+      Class.new(ActiveRecord::Base).tap{ |model| model.table_name = name }
+    rescue ActiveRecord::ConnectionNotEstablished => e
+      new_e = ActiveRecord::ConnectionNotEstablished.new("#{e.message} (maybe you should call `Qdb.setup` first)")
+      new_e.set_backtrace(e.backtrace)
+      raise new_e
+    end
+  end
+
 end
